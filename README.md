b77d4be9a496c3f9ccc0f75ed1b3bbf72394e124
chrishart0 <chris@chris.chris>
2021-03-18 11:18:01 -0400

    basic working tests p95

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tdd-by-example-beck-practice
+I am following along with Kent beck in Section 2 of Test Driven Development by Example
---
+++ run_tests.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+class WasRun:
+    def __init__(self, name):
+        self.wasRun= None
+
+    def testMethod(self): 
+        self.wasRun= 1
+
+    def run(self):
+        self.testMethod()
+
+
+test= WasRun("TestMethod")
+print (test.wasRun)
+test.run()
+print (test.wasRun)
